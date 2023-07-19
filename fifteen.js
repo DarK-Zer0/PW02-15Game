084dcdc4d94aa8261872f37356b5db2a92981a16
Indra Meyers <67623845+DarK-Zer0@users.noreply.github.com>
2023-07-18 20:41:25 -0400

    Added click & swapping cell functionality

--- fifteen.js
+++ fifteen.js
@@ -8,11 +8,11 @@ function createBoard () {
             // Makes a new div w/ a class: cell
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            // Stores the correct location of the cell
+            // Stores the location of the cell
             cell.setAttribute('data-row',row);
             cell.setAttribute('data-column',col);
             if (row == 3 && col == 3) { // Adds cell 16 to the board and makes it empty
-                cell.setAttribute('data-empty', '1');
+                cell.setAttribute('data-empty','1');
                 table.appendChild(cell);
             } else { // Adds cells 1-15 to the board
                 // Gives each cell a number to display over themself
@@ -20,6 +20,10 @@ function createBoard () {
                 cell.appendChild(number);
                 table.appendChild(cell);
             }
+            // Adds an event listener to the cell that calls the swapCell function when clicked
+            cell.addEventListener('click', () => {
+                swapCell(cell);
+            });
         }
     }
 
@@ -30,11 +34,11 @@ function createBoard () {
         const col = cell.dataset.column;
         // Sets the portion of the image the selected cell will contain by default
         cell.setAttribute('data-img',`${row}${col}`);
-        cell.style.backgroundPosition = imgPos(cell);
+        loadImg(cell);
     });
 }
 
-// Retrieves the portion of the image of a cell based on its stored data-img attribute
+// Retrieves the section of the image a cell should hold based on its stored data-img attribute
 function imgPos (cell) {
     let imgData = cell.getAttribute('data-img');
     let col = parseInt(imgData[0]);
@@ -44,8 +48,12 @@ function imgPos (cell) {
 
     return `${x}px ${y}px`;
 }
+// Loads the image section based on the stored data-img
+function loadImg (cell) {
+    cell.style.backgroundPosition = imgPos(cell);
+}
 
-// Detects if a neighbor cell is the empty cell and returns a data-img value 
+// Detects if a neighbor cell is the empty cell and returns a string containing the position of empty cell if true 
 function nearEmpty (cell) {
     const emptyCell = document.querySelector('[data-empty]');
     const emptyRow = parseInt(emptyCell.dataset.row);
@@ -62,5 +70,32 @@ function nearEmpty (cell) {
     if (rowDisp > 1 || colDisp > 1) { // Checks if given cell and empty cell are not adjacent
         return null;
     }
+    if (rowDisp == 1 && colDisp == 1) { // Checks if given cell and empty cell are diagonally adjacent
+        return null;
+    }
     return `${emptyRow}${emptyCol}`;
+}
+// Swaps a non-empty cell with an empty cell
+function swapCell (cell) {
+    const emptyPos = nearEmpty(cell);
+    if (emptyPos == null) { // Does nothing if empty cell is not found adjacent to non-empty cell
+        return;
+    }
+
+    // Swaps the data-empty attribute, moving the blank space
+    const emptyCell = document.querySelector(`[data-row="${emptyPos[0]}"][data-column="${emptyPos[1]}"]`);
+    emptyCell.removeAttribute('data-empty');
+    cell.setAttribute('data-empty','1');
+
+    // Swaps the data-img attribute
+    const noImg = emptyCell.dataset.img;
+    emptyCell.setAttribute('data-img',cell.dataset.img);
+    cell.setAttribute('data-img',noImg);
+        // Loads the new image section
+        loadImg(emptyCell);
+
+    // Swaps the number of the cell
+    const number = cell.textContent;
+    emptyCell.textContent = number;
+    cell.textContent = '';
 }
